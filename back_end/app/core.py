fef3a6c12e17fa9ff9e9a10df401caf4d365df08
tauraspaj <pajuodis.tauras@gmail.com>
2023-03-20 20:05:51 +0000

    back_end/app/core.py: Fix imports

--- back_end/app/core.py
+++ back_end/app/core.py
@@ -1,18 +1,9 @@
-import logging
-import math
-
-from .inferences.model_predictions import run_prediction
-from .common.weather_api import weather_pipe
-
-import config
-
-logging.basicConfig(level=config.LOGLEVEL)
-logger = logging.getLogger(__name__)
+from .common import weather_api
+from .inferences import model_predictions
 
 
 def get_predictions():
-
-    raw_weather, proc_weather = weather_pipe()
-    preds = run_prediction(proc_weather, raw_weather)
+    raw_weather, proc_weather = weather_api.weather_pipe()
+    preds = model_predictions.run_prediction(proc_weather, raw_weather)
 
     return preds
