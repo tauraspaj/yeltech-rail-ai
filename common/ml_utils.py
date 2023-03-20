6da953985f15ec25527578ecf5fc039fc572aa6f
tauraspaj <pajuodis.tauras@gmail.com>
2023-03-20 19:32:44 +0000

    common/ml_utils.py: Raise exception if file does not exist

--- common/ml_utils.py
+++ common/ml_utils.py
@@ -1,9 +1,17 @@
-import pickle
 import os.path
-from ..paths import ROOT_DIR
+import pickle
+import errno
+
+from .paths import ROOT_DIR
 
 
 def load_model(model_name):
     model_path = os.path.join(ROOT_DIR, 'assets/models', model_name)
+
+    # Check if path exists
+    if not os.path.exists(model_path):
+        raise FileNotFoundError(
+            errno.ENOENT, os.strerror(errno.ENOENT), model_path)
+
     model = pickle.load(open(model_path, 'rb'))
     return model
