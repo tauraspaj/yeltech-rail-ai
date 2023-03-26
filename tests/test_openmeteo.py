7c8935e5d33809b4117b3909f5f31d325fb4d6e3
tauraspaj <pajuodis.tauras@gmail.com>
2023-03-26 22:05:00 +0100

    tests/test_openmeteo.py: Refactor using constants

--- tests/test_openmeteo.py
+++ tests/test_openmeteo.py
@@ -3,6 +3,12 @@ import requests
 
 from common.weather_api import OpenMeteo
 
+# Reusable constants
+LATITUDE = 51.5072
+LONGITUDE = 0.1276
+START_DATE = '2023-03-20'
+DAYS_AHEAD = 1
+
 
 class TestOpenMeteo:
     @pytest.fixture
@@ -13,10 +19,11 @@ class TestOpenMeteo:
         assert openMeteo.base_url == 'https://api.open-meteo.com/v1/forecast?'
 
     def testApiUrl(self, openMeteo):
-        api_url = openMeteo.generate_api_url(51.5072, 0.1276, '2023-03-20', 7)
+        api_url = openMeteo.generate_api_url(LATITUDE, LONGITUDE,
+                                             START_DATE, DAYS_AHEAD)
         assert api_url == 'https://api.open-meteo.com/v1/forecast?' \
             'latitude=51.5072&longitude=0.1276' \
-            '&start_date=2023-03-20&end_date=2023-03-27' \
+            '&start_date=2023-03-20&end_date=2023-03-21' \
             '&hourly=temperature_2m,relativehumidity_2m,dewpoint_2m,' \
             'apparent_temperature,precipitation,cloudcover,cloudcover_low,' \
             'cloudcover_mid,cloudcover_high,shortwave_radiation,' \
@@ -24,12 +31,14 @@ class TestOpenMeteo:
             'diffuse_radiation,direct_normal_irradiance'
 
     def testApiUrlStatus(self, openMeteo):
-        api_url = openMeteo.generate_api_url(51.5072, 0.1276, '2023-03-20', 7)
+        api_url = openMeteo.generate_api_url(LATITUDE, LONGITUDE,
+                                             START_DATE, DAYS_AHEAD)
         status_code = requests.get(api_url).status_code
         assert status_code == 200
 
     def testApiJsonData(self, openMeteo):
-        json_data = openMeteo.fetch_api_data(51.5072, 0.1276, '2023-03-20', 1)
+        json_data = openMeteo.fetch_api_data(LATITUDE, LONGITUDE,
+                                             START_DATE, DAYS_AHEAD)
         assert len(json_data) == 9
         assert list(json_data.keys()) == [
             'latitude', 'longitude', 'generationtime_ms',
@@ -50,8 +59,9 @@ class TestOpenMeteo:
         assert len(json_data['hourly']['relativehumidity_2m']) == 48
 
     def testJsonDataProcessing(self, openMeteo):
-        json_data = openMeteo.fetch_api_data(51.5072, 0.1276, '2023-03-20', 1)
-        data_df = openMeteo.process_json_data(json_data)
+        json_data = openMeteo.fetch_api_data(LATITUDE, LONGITUDE,
+                                             START_DATE, DAYS_AHEAD)
+        data_df = openMeteo.add_manual_fields(json_data)
         assert 'month' in list(data_df.columns)
         assert 'day_of_year' in list(data_df.columns)
         assert 'hour_of_day' in list(data_df.columns)
