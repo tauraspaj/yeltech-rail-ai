bc4dd93e850d7340de1224e41fbf3a0ee9f656ac
tauraspaj <pajuodis.tauras@gmail.com>
2023-03-26 22:01:49 +0100

    schema.sql: Add a list of parameters used for each model

--- schema.sql
+++ schema.sql
@@ -7,6 +7,21 @@ CREATE TABLE models (
     model_file_name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE prediction_parameters (
+    _parameter_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    parameter_name VARCHAR(255) NOT NULL,
+    unit VARCHAR(255),
+    param_provider VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE model_parameters (
+    _model_parameter_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    model_id INT UNSIGNED NOT NULL,
+    prediction_parameter_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (model_id) REFERENCES models(_model_id),
+    FOREIGN KEY (prediction_parameter_id) REFERENCES prediction_parameters(_parameter_id)
+);
+
 CREATE TABLE devices (
     _device_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
     device_name VARCHAR(255) NOT NULL,
@@ -30,16 +45,8 @@ CREATE TABLE predictions (
     FOREIGN KEY (device_id) REFERENCES devices(_device_id)
 );
 
-CREATE TABLE prediction_parameters (
-    _parameter_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    parameter_name VARCHAR(255) NOT NULL,
-    unit VARCHAR(255),
-    param_provider VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE parameter_history (
     _parameter_history_id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    is_used BOOLEAN DEFAULT 0,
     parameter_value FLOAT DEFAULT NULL,
     parameter_id INT UNSIGNED,
     prediction_id INT UNSIGNED,
@@ -69,3 +76,7 @@ VALUES
     ('direct_radiation', 'w/m2', 'Open-Meteo'),
     ('diffuse_radiation', 'w/m2', 'Open-Meteo'),
     ('direct_normal_irradiance', 'w/m2', 'Open-Meteo');
+
+INSERT INTO devices(device_name, prediction_status, latitude, longitude, model_id) VALUES ('TEST RTMU', 1, 51.5072, 0.1276, 1);
+INSERT INTO model_parameters(model_id, prediction_parameter_id) 
+	(1, 1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7), (1,8), (1,9), (1,10), (1,11), (1,12), (1, 13), (1,14), (1,15), (1,16), (1,17), (1,18), (1,19), (1,20);
