78627edae6b7448f7358603126242a44980b83f3
tauraspaj <pajuodis.tauras@gmail.com>
2023-03-29 17:09:53 +0100

    common/utils.py: Param list to dict converter

--- common/utils.py
+++ common/utils.py
@@ -3,12 +3,12 @@ import os.path
 import pickle
 from datetime import datetime, timedelta
 
+from common import sql
 from common.paths import MODELS_DIR
-from common.sql import Models
 
 
 def load_model(model_id):
-    return fetch_model(Models().get_one(model_id)['model_file_name'])
+    return fetch_model(sql.Models().get_one(model_id)['model_file_name'])
 
 
 def fetch_model(model_name):
@@ -27,3 +27,10 @@ def calculate_time_ahead(start_date, days_ahead):
     start_date_object = datetime.strptime(start_date, '%Y-%m-%d').date()
     end_date_object = start_date_object + timedelta(days=days_ahead)
     return str(start_date_object), str(end_date_object)
+
+
+def convert_model_param_list_to_dict(params):
+    id_to_param_dict = {}
+    for param in params:
+        id_to_param_dict[param['parameter_name']] = param['_parameter_id']
+    return id_to_param_dict
