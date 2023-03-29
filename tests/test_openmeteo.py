f9ac7fe11811fb9ee73c0c7e2802787bffe8ed4e
tauraspaj <pajuodis.tauras@gmail.com>
2023-03-29 17:09:00 +0100

    tests/test_openmeteo.py: Update function names

--- tests/test_openmeteo.py
+++ tests/test_openmeteo.py
@@ -9,16 +9,26 @@ LONGITUDE = 0.1276
 START_DATE = '2023-03-20'
 DAYS_AHEAD = 1
 
+device_data = {
+    '_device_id': 1,
+    'device_name': 'TEST RTMU',
+    'prediction_status': 1,
+    'latitude': 52.956240,
+    'longitude': -1.184660,
+    'model_id': 2,
+    'depo_location': 'Nottingham'
+}
+
 
 class TestOpenMeteo:
     @pytest.fixture
     def openMeteo(self):
         return OpenMeteo()
 
-    def testInit(self, openMeteo):
+    def test_init(self, openMeteo):
         assert openMeteo.base_url == 'https://api.open-meteo.com/v1/forecast?'
 
-    def testApiUrl(self, openMeteo):
+    def test_generate_api_url(self, openMeteo):
         api_url = openMeteo.generate_api_url(LATITUDE, LONGITUDE,
                                              START_DATE, DAYS_AHEAD)
         assert api_url == 'https://api.open-meteo.com/v1/forecast?' \
@@ -30,13 +40,13 @@ class TestOpenMeteo:
             'et0_fao_evapotranspiration,windspeed_10m,direct_radiation,' \
             'diffuse_radiation,direct_normal_irradiance'
 
-    def testApiUrlStatus(self, openMeteo):
+    def test_api_url_status(self, openMeteo):
         api_url = openMeteo.generate_api_url(LATITUDE, LONGITUDE,
                                              START_DATE, DAYS_AHEAD)
         status_code = requests.get(api_url).status_code
         assert status_code == 200
 
-    def testApiJsonData(self, openMeteo):
+    def test_fetch_api_data(self, openMeteo):
         json_data = openMeteo.fetch_api_data(LATITUDE, LONGITUDE,
                                              START_DATE, DAYS_AHEAD)
         assert len(json_data) == 9
@@ -58,10 +68,10 @@ class TestOpenMeteo:
         assert len(json_data['hourly']['temperature_2m']) == 48
         assert len(json_data['hourly']['relativehumidity_2m']) == 48
 
-    def testJsonDataProcessing(self, openMeteo):
+    def test_add_manual_fields(self, openMeteo):
         json_data = openMeteo.fetch_api_data(LATITUDE, LONGITUDE,
                                              START_DATE, DAYS_AHEAD)
-        data_df = openMeteo.add_manual_fields(json_data)
+        data_df = openMeteo.add_manual_fields(json_data, device_data)
         assert 'month' in list(data_df.columns)
         assert 'day_of_year' in list(data_df.columns)
         assert 'hour_of_day' in list(data_df.columns)
