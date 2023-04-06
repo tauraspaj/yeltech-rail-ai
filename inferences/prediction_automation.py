1203ca9498c0ca36fb1adacfeaa89080fd59916c
tauraspaj <pajuodis.tauras@gmail.com>
2023-04-06 21:51:41 +0100

    inferences/prediction_automation.py: Pipeline logging

--- inferences/prediction_automation.py
+++ inferences/prediction_automation.py
@@ -1,6 +1,11 @@
-from inferences.model_predictions import prediction_pipeline
-from common.sql import Devices
 from datetime import date
+
+from common import logger
+from common.sql import Devices
+from inferences.model_predictions import prediction_pipeline
+
+logger = logger.setup_logger()
+
 
 # Fetch all device ids from the database
 device_ids = Devices().get_all()
@@ -8,11 +13,14 @@ device_ids = Devices().get_all()
 start_date = date.today()
 
 for device in device_ids:
-    print(f"\n[DiD: {device['_device_id']}] Starting prediction pipeline.")
+    logger.info('|===== [DiD: %d] Starting prediction pipeline =====|',
+                device['_device_id'])
 
     if device['prediction_status'] == 0:
-        print(f"[DiD: {device['_device_id']}] Prediction status 0. Skipping..")
+        logger.info('[DiD: %d]}] Prediction status 0. Skipping..',
+                    device['_device_id'])
         continue
 
     prediction_pipeline(device['_device_id'], str(start_date), days_ahead=6)
-    print(f"[DiD: {device['_device_id']}] Prediction pipeline finished!")
+    logger.info('[DiD: %d] Prediction pipeline finished!',
+                device['_device_id'])
