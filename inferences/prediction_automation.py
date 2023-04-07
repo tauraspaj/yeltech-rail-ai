40d143c22bc732fdd06729e6b6aa050e1aeb460c
tauraspaj <pajuodis.tauras@gmail.com>
2023-04-07 12:32:11 +0100

    inferences/prediction_automation.py: Implement logging

--- inferences/prediction_automation.py
+++ inferences/prediction_automation.py
@@ -1,26 +1,34 @@
+import sys
 from datetime import date
 
 from common import logger
 from common.sql import Devices
 from inferences.model_predictions import prediction_pipeline
 
-logger = logger.setup_logger()
+log = logger.get_logger()
 
+log.info("====================== PREDICTION AUTOMATION ======================")
 
 # Fetch all device ids from the database
-device_ids = Devices().get_all()
+try:
+    device_ids = Devices().get_all()
+except Exception as e:
+    log.error(
+        "Problem fetching device ids. "
+        "Ending process. Error: %s", e)
+    sys.exit()
 
+# Fetch todays date
 start_date = date.today()
 
 for device in device_ids:
-    logger.info('|===== [DiD: %d] Starting prediction pipeline =====|',
-                device['_device_id'])
+    log.info('------ [DiD: %d] Starting prediction pipeline ------',
+             device['_device_id'])
 
     if device['prediction_status'] == 0:
-        logger.info('[DiD: %d]}] Prediction status 0. Skipping..',
+        log.warning('[DiD: %d]}] Prediction status 0. Skipping...',
                     device['_device_id'])
         continue
 
     prediction_pipeline(device['_device_id'], str(start_date), days_ahead=6)
-    logger.info('[DiD: %d] Prediction pipeline finished!',
-                device['_device_id'])
+    log.info('[DiD: %d] Prediction pipeline finished.', device['_device_id'])
