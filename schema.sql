70af46280741904de0960f475b0209cb12fb29fb
tauraspaj <pajuodis.tauras@gmail.com>
2023-04-13 14:40:15 +0100

    schema.sql: Ignore global model

--- schema.sql
+++ schema.sql
@@ -99,8 +99,6 @@ INSERT INTO model_parameters(model_id, prediction_parameter_id) VALUES
 	(5, 1), (5,2), (5,3), (5,4), (5,5), (5,6), (5,7), (5,8), (5,9), (5,10), (5,11), (5,12), (5, 13), (5,14), (5,15), (5,16), (5,17), (5,18), (5,19), (5,20);
 
 INSERT INTO devices(device_name, prediction_status, latitude, longitude, model_id, depo_location) VALUES ('RTMU 3280', 1, 50.875246, -1.264611, 1, 'Eastleigh');
-INSERT INTO devices(device_name, prediction_status, latitude, longitude, model_id, depo_location) VALUES ('RTMU 3280', 1, 50.875246, -1.264611, 2, 'Eastleigh');
-INSERT INTO devices(device_name, prediction_status, latitude, longitude, model_id, depo_location) VALUES ('RTMU 2716', 1, 52.956240, -1.184660, 2, 'Nottingham');
 INSERT INTO devices(device_name, prediction_status, latitude, longitude, model_id, depo_location) VALUES ('RTMU 2716', 1, 52.956240, -1.184660, 3, 'Nottingham');
 INSERT INTO devices(device_name, prediction_status, latitude, longitude, model_id, depo_location) VALUES ('RTMU 2716', 1, 52.956240, -1.184660, 4, 'Nottingham');
 INSERT INTO devices(device_name, prediction_status, latitude, longitude, model_id, depo_location) VALUES ('RTMU 2716', 1, 52.956240, -1.184660, 5, 'Nottingham');
