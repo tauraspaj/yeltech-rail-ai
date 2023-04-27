19d2b3a9c2ccb3dddae3b96b4497616ee1620c0d
tauraspaj <pajuodis.tauras@gmail.com>
2023-04-27 16:45:41 +0100

    schema.sql: Add 50q model

--- schema.sql
+++ schema.sql
@@ -86,6 +86,7 @@ INSERT INTO models(model_file_name, param_provider) VALUES ('baseline_global_mod
 INSERT INTO models(model_file_name, param_provider) VALUES ('baseline_global_lgbm_mean_model.pkl', 'Open-Meteo');
 INSERT INTO models(model_file_name, param_provider) VALUES ('baseline_global_lgbm_95q_model.pkl', 'Open-Meteo');
 INSERT INTO models(model_file_name, param_provider) VALUES ('baseline_global_lgbm_5q_model.pkl', 'Open-Meteo');
+INSERT INTO models(model_file_name, param_provider) VALUES ('baseline_global_lgbm_50q_model.pkl', 'Open-Meteo');
 
 INSERT INTO model_parameters(model_id, prediction_parameter_id) VALUES
 	(1, 1), (1,2), (1,3), (1,4), (1,5), (1,6), (1,7), (1,8), (1,9), (1,10), (1,11), (1,12), (1, 13), (1,14), (1,15), (1,16), (1,17), (1,18), (1,19), (1,20), (1,21);
@@ -97,8 +98,11 @@ INSERT INTO model_parameters(model_id, prediction_parameter_id) VALUES
 	(4, 1), (4,2), (4,3), (4,4), (4,5), (4,6), (4,7), (4,8), (4,9), (4,10), (4,11), (4,12), (4, 13), (4,14), (4,15), (4,16), (4,17), (4,18), (4,19), (4,20);
 INSERT INTO model_parameters(model_id, prediction_parameter_id) VALUES
 	(5, 1), (5,2), (5,3), (5,4), (5,5), (5,6), (5,7), (5,8), (5,9), (5,10), (5,11), (5,12), (5, 13), (5,14), (5,15), (5,16), (5,17), (5,18), (5,19), (5,20);
+INSERT INTO model_parameters(model_id, prediction_parameter_id) VALUES
+	(6, 1), (6,2), (6,3), (6,4), (6,5), (6,6), (6,7), (6,8), (6,9), (6,10), (6,11), (6,12), (6, 13), (6,14), (6,15), (6,16), (6,17), (6,18), (6,19), (6,20);
 
 INSERT INTO devices(device_name, prediction_status, latitude, longitude, model_id, depo_location) VALUES ('RTMU 3280', 1, 50.875246, -1.264611, 1, 'Eastleigh');
 INSERT INTO devices(device_name, prediction_status, latitude, longitude, model_id, depo_location) VALUES ('RTMU 2716', 1, 52.956240, -1.184660, 3, 'Nottingham');
 INSERT INTO devices(device_name, prediction_status, latitude, longitude, model_id, depo_location) VALUES ('RTMU 2716', 1, 52.956240, -1.184660, 4, 'Nottingham');
 INSERT INTO devices(device_name, prediction_status, latitude, longitude, model_id, depo_location) VALUES ('RTMU 2716', 1, 52.956240, -1.184660, 5, 'Nottingham');
+INSERT INTO devices(device_name, prediction_status, latitude, longitude, model_id, depo_location) VALUES ('RTMU 2716', 1, 52.956240, -1.184660, 6, 'Nottingham');
