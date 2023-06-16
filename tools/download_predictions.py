896263efb13c6fa32ba2999c120608db9526f16a
lucamoscattini1 <luca_moscattini@yahoo.co.uk>
2023-06-16 16:15:33 +0100

    tools/: Add prediction download functionality

---
+++ tools/download_predictions.py
@@ -0,0 +1,23 @@
+import csv
+import os
+import mysql.connector
+
+# connect to the database
+db_connection = mysql.connector.connect(
+    host=os.getenv('DB_ENDPOINT'),
+    user=os.getenv('DB_USERNAME'),
+    password=os.getenv('DB_PASSWORD'),
+    database='yeltech_ai_db'
+)
+
+# query the database and get the results
+query = 'SELECT * FROM predictions'
+
+cursor = db_connection.cursor()
+cursor.execute(query)
+results = cursor.fetchall()
+
+# save results to a csv
+with open("predictions.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerows(results)
